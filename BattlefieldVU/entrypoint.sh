02928c9166ebfe0aaf3a8973c3db2853d6966e27
Evan <whatcs@jointheb.org>
2023-04-14 00:41:12 -0700

    fixin fuckups

--- BattlefieldVU/entrypoint.sh
+++ BattlefieldVU/entrypoint.sh
@@ -5,8 +5,6 @@ echo "Running on Debian $(cat /etc/debian_version)"
 echo "Current timezone: $(cat /etc/timezone)"
 wine --version
 
-wine vu/client/vu.com -gamepath bf3 -activate -o_mail ${EA_EMAIL} -o_pass ${EA_PASSWORD}
-
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
