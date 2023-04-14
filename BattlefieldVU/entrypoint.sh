160918bed62508536a3517a12f11ee2a57123397
Evan <whatcs@jointheb.org>
2023-04-13 21:41:20 -0700

    initial bb

---
+++ BattlefieldVU/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Information output
+echo "Running on Debian $(cat /etc/debian_version)"
+echo "Current timezone: $(cat /etc/timezone)"
+wine --version
+
+mkdir tmp
+cd tmp/
+wget -nc https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x86.exe
+xvfb-run wine tmp/vcredist_x86.exe /q
+cd ../
+
+mkdir -p vu/{client,instance}
+cd vu/client
+wget https://veniceunleashed.net/files/vu.zip
+unzip vu.zip
+rm vu.zip
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
