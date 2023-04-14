6336db48699850e2b19350078fec8aabf9ef1fc5
Evan <whatcs@jointheb.org>
2023-04-14 01:37:42 -0700

    debug bb

--- BattlefieldVU/entrypoint.sh
+++ BattlefieldVU/entrypoint.sh
@@ -5,6 +5,7 @@ echo "Running on Debian $(cat /etc/debian_version)"
 echo "Current timezone: $(cat /etc/timezone)"
 wine --version
 
+ls -l /mnt
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
