a9f337b81b975cad00e124b11b7a1ca0aa829142
Evan <whatcs@jointheb.org>
2023-04-13 23:34:04 -0700

    updates

--- BattlefieldVU/entrypoint.sh
+++ BattlefieldVU/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 cd /home/container
 
-# Make internal Docker IP address available to processes.
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
-
 # Information output
 echo "Running on Debian $(cat /etc/debian_version)"
 echo "Current timezone: $(cat /etc/timezone)"
